376491a9dacf2caa893caaa4ce28c4e735856fed
tigran17 <karapetyan.tigran1993@gmail.com>
2019-01-14 21:35:09 -0800

    Sart GoldJack Project
    Start GoldJackProject

---
+++ GoldJack/Controllers/Api/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoldJack.Controllers.Api
+{
+    public class GameController : Controller
+    {
+        [HttpPost]
+        public void StartGame()
+        {
+
+        }
+
+        [HttpGet]
+        public int GetCoinValue()
+        {
+            return 0;
+        }
+
+        [HttpGet]
+        public void GetRange()
+        {
+
+        }
+    }
+}
---
+++ GoldJack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoldJack.MvcApplication" Language="C#" %>
